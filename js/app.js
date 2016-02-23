794e67ba6c5555e260b461f30d36e42ddb6f4704
Mike Wickwar <mike.wickwar@gmail.com>
2016-02-23 16:52:05 -0700

    working movies/show route with movies displaying

--- js/app.js
+++ js/app.js
@@ -2,17 +2,9 @@ var app = angular.module('movieSearch', ['ngRoute']);
 
 app.config(function($routeProvider) {
     $routeProvider
-      .when('/', {
-        controller: 'MainController',
-        template: '<h1>You have reached the main controller</h1>'
-      })
-      .when('/goodbye', {
-        templateUrl: './js/partials/goodbye.html',
-        controller: 'GoodByeController'
-      })
-      .when('/hello', {
-        templateUrl: './js/partials/hello.html',
-        controller: 'HelloController'
+      .when('/movies/show', {
+        templateUrl: './js/partials/showmovies.html',
+        controller: 'MainController'
       })
       .otherwise({
         template: '<div><h1>No Page Located Here</h1></div>'
