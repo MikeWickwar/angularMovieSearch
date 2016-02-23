cd246d230137a74e45f14e3c4c777e89fe8fe8ef
Mike Wickwar <mike.wickwar@gmail.com>
2016-02-23 15:23:17 -0700

    working search on first api call now same package bug fixed

--- js/controllers/main_controller.js
+++ js/controllers/main_controller.js
@@ -5,7 +5,7 @@ app.controller('MainController', ['$scope','$http', function ($scope, $http) {
     $scope.searchme = $scope.title;
     console.log($scope.searchme);
 
-    $http.get('http://www.omdbapi.com/?t='+$scope.searchme+'&y=&plot=short&r=json').then(function(movieSearch){
+    $http.get('http://www.omdbapi.com/?s='+$scope.searchme+'&y=&plot=short&r=json').then(function(movieSearch){
       console.log(movieSearch);
       $scope.movie = movieSearch.data;
       $scope.movieId = movieSearch.data.imdbID;
