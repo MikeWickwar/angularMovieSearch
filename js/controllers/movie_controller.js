2f2b0c4575e0f5e035f4d36635877c693537fe35
Mike Wickwar <mike.wickwar@gmail.com>
2016-02-24 20:24:33 -0700

    mvps completed

--- js/controllers/movie_controller.js
+++ js/controllers/movie_controller.js
@@ -5,6 +5,15 @@ app.controller('singleMovieController', ['$scope','$http','$route', function ($s
     $http.get('http://www.omdbapi.com/?i='+$route.current.params.id+'&y=&plot=full&r=json').then(function(movieSearch){
       $scope.movieinfo = movieSearch.data
       $scope.title = movieSearch.data.Title
+      $scope.actors = movieSearch.data.Actors
+      $scope.plot = movieSearch.data.Plot
+      $scope.poster = movieSearch.data.poster
+      delete $scope.movieinfo.Type
+      delete $scope.movieinfo.Response
+      delete $scope.movieinfo.Title
+      delete $scope.movieinfo.Actors
+      delete $scope.movieinfo.Plot
+      delete $scope.movieinfo.Poster
       console.log($scope.movieinfo);
     });
   }
